6a7cc8ad195bf05996d00752eaeb382a122f1b45
Arthur Victor <140616974+arthurvicttor@users.noreply.github.com>
2026-04-17 22:07:57 -0300

    feat: adiciona novas rotas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
-// import Home from "./pages/Home/Home";
-// import Universes from "./pages/Universes/Universes";
-// import UniverseDetail from "./pages/UniverseDetail/UniverseDetail";
-// import Characters from "./pages/Characters/Characters";
-// import CharacterDetail from "./pages/CharacterDetail/CharacterDetail";
-// import ComicDetail from "./pages/ComicDetail/ComicDetail";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import Universes from "./pages/Universes/Universes";
+import UniverseDetail from "./pages/UniverseDetail/UniverseDetail";
+import Characters from "./pages/Characters/Characters";
+import CharacterDetail from "./pages/CharacterDetail/CharacterDetail";
+import ComicDetail from "./pages/ComicDetail/ComicDetail";
 import "./App.css";
 
 function App() {
@@ -14,12 +15,13 @@ function App() {
       <Header />
       <main className="main-content">
         <Routes>
-          {/* <Route path="/" element={<Home />} /> */}
-          {/* <Route path="/universes" element={<Universes />} /> */}
-          {/* <Route path="/universes/:id" element={<UniverseDetail />} /> */}
-          {/* <Route path="/characters" element={<Characters />} /> */}
-          {/* <Route path="/characters/:id" element={<CharacterDetail />} /> */}
-          {/* <Route path="/comics/:id" element={<ComicDetail />} /> */}
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/universes" element={<Universes />} />
+          <Route path="/universes/:id" element={<UniverseDetail />} />
+          <Route path="/characters" element={<Characters />} />
+          <Route path="/characters/:id" element={<CharacterDetail />} />
+          <Route path="/comics/:id" element={<ComicDetail />} />
         </Routes>
       </main>
     </div>
